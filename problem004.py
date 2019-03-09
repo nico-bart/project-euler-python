5e1316f8c7e8aeec736037abe7899bf54251680a
nico-bart <nico.scholz@scholzsue.de>
2019-03-09 19:31:18 +0100

    put prime numbers in external file

--- problem004.py
+++ problem004.py
@@ -1,16 +1,18 @@
-# list of all 3 digit numbers:
+# A palindromic number reads the same both ways. The largest palindrome made
+# from the product of two 2-digit numbers is 9009 = 91 × 99.
+# Find the largest palindrome made from the product of two 3-digit numbers.
+
 threeDigits = []
 biggestPalindrome = 0
 
 # build a list of all multiples of three digit numbers
 for i in range(100, 999):
     for j in range(i, 999):
-        #threeDigits.append( (i, j, i*j) )
         threeDigits.append( i*j )
 
 # convert each number to string
-# check if number-string has an uneven lenght. if so i cant be a palindrome
-# else check if actual palindrome
+# check if number-string has an uneven lenght. If so it cant be a palindrome
+# else check if its an actual palindrome
 for k in range(0, len(threeDigits)):
 
     print(k,"st entry yields ", threeDigits[k])
--- problem005.py
+++ problem005.py
@@ -1,3 +1,12 @@
+# 2520 is the smallest number that can be divided by each of the numbers from
+# 1 to 10 without any remainder.
+# What is the smallest positive number that is evenly divisible by all of the
+# numbers from 1 to 20?
+#
+# Finde das kleinste gemeinsame Vielfache der Zahlen 1 bis 20
+#
+# One has to find all the prime factors of the numbers 1 to 20
+
 # factors
 # 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
 #
