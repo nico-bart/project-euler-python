65112dc4eab5d688fae9f02da8adff6483ce8cf6
nico-bart <nico.scholz@scholzsue.de>
2019-03-10 16:32:39 +0100

    kind of failing in reading files in c++

--- problem008.py
+++ problem008.py
@@ -1,4 +1,5 @@
-# The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
+# The four adjacent digits in the 1000-digit
+# number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
 #
 # 73167176531330624919225119674426574742355349194934
 # 96983520312774506326239578318016984801869478851843
@@ -21,7 +22,8 @@
 # 05886116467109405077541002256983155200055935729725
 # 71636269561882670428252483600823257530420752963450
 #
-# Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product?
+# Find the thirteen adjacent digits in the 1000-digit number that have the
+# greatest product. What is the value of this product?
 #
 
 
@@ -36,8 +38,7 @@ newstring = string.replace('\n', '')
 biggestProd = 1
 for i in range(0, len(newstring)-12):
     shortstring = newstring[i:(i+13)]
-
-
+    
     prod = 1
     for j in range(0, len(shortstring)):
         if shortstring[j] == 0:
